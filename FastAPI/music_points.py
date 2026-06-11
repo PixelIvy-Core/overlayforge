d43422ef047d13fe470e83d2c06b139326e7be30
SkyeJxn <78538146+SkyeJxn@users.noreply.github.com>
2026-06-11 22:07:30 +0200

    feat: enhance music queue handling and add endpoint to retrieve all tracks

--- FastAPI/music_points.py
+++ FastAPI/music_points.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter, HTTPException
 from collections.abc import AsyncIterable
 from fastapi.sse import EventSourceResponse, ServerSentEvent
-from connections import get_redis, get_gonic_url
+from connections import get_redis, get_gonic_url, get_gonic_info
 from models import TrackInfo
 from config import *
 import asyncio as asc
-import requests as req
 
 router = APIRouter(prefix="/music", tags=["music"])
 
@@ -34,7 +33,13 @@ async def get_playing_queue():
             raise HTTPException(503, "Redis not reachable")
         queue = r.lrange(REDIS_QUEUE,0,-1)
         r.close()
-    return queue
+    queue_info = []
+    for id in queue:
+        url = get_gonic_url("getSong", id)
+        song = get_gonic_info(url, "song")
+        queue_info.append(song)
+    simplified = [{"id": track["id"], "title": str(track["title"]).replace(".mp3",""), "artist": track["artist"]} for track in queue_info]
+    return simplified
 
 @router.post("/clearQueue")
 async def clear_current_queue():
@@ -54,22 +59,32 @@ async def next_track():
         r.close()
     if next_id:
         url = get_gonic_url("getSong", next_id)
-        response = req.request("GET", url, params={"f": "json"})
-        payload = response.json()["subsonic-response"]
-        metadata = payload["song"]
+        metadata = get_gonic_info(url, "song")
         setTrackInfo(metadata["id"], str.replace(metadata['title'], ".mp3", ""), metadata['artist'], metadata['album'])
     else:
         raise HTTPException(204, "no more songs in Queue")
         
     return {"status": 200, "message": "next track set"}
 
-@router.get("/nowPlaying")
+@router.get("/nowPlaying", response_class=EventSourceResponse)
 async def get_current_track_informations():
+    current_track = None
     with get_redis() as r:
         if not r:
-            raise HTTPException(503, "Redis not reachable")
+            yield ServerSentEvent(comment="Redis not reachable")
         info = r.hgetall(REDIS_INFO)
-    return info
+    if info != current_track:
+        yield ServerSentEvent(data=info)
+        current_track = info
+    while True:
+        await asc.sleep(5)
+        with get_redis() as r:
+            if not r:
+                yield ServerSentEvent(comment="Redis not reachable")
+            info = r.hgetall(REDIS_INFO)
+        if info != current_track:
+            yield ServerSentEvent(data=info)
+            current_track = info
 
 @router.post("/setState")
 async def set_play_state(new_state: bool):
@@ -84,7 +99,7 @@ async def set_play_state(new_state: bool):
 async def get_play_state():
     last_state = None
     while True:
-        await asc.sleep(3)
+        await asc.sleep(1)
         with get_redis() as r:
             if not r:
                 yield ServerSentEvent(comment="Redis not reachable")
@@ -119,3 +134,20 @@ async def get_track_info(curr_id: str) -> AsyncIterable[TrackInfo]:
             if current_id != last_track_id:
                 yield ServerSentEvent(data=track)
                 last_track_id = current_id
+                
+@router.get("/getAllTracks")
+async def get_all_available_tracks():
+    index_url = get_gonic_url("getIndexes")
+    response = get_gonic_info(index_url, "indexes")
+    raw = response["index"]
+    indexes = [artist["id"] for item in raw for artist in item.get("artist", [])]
+    tracks = []
+    for index in indexes:
+        url = get_gonic_url("getMusicDirectory", index)
+        dir_response = get_gonic_info(url, "directory")
+        info = dir_response["child"]
+        simplified = [{"id": track["id"], "title": str(track["title"]).replace(".mp3",""), "artist": track["artist"]} for track in info]
+        if dir_response:
+            tracks.append(simplified)
+    output = [item for sublist in tracks for item in sublist]
+    return output
