25d6435717e4c6b3a08f970160bbdc91fe7e3aef
Skye Jonczik <jeanjonczik2003@gmail.com>
2026-06-13 21:29:47 +0200

    feat: enhance MusicBox component with animation effects for track visibility

--- vite/src/components/Music.jsx
+++ vite/src/components/Music.jsx
@@ -1,17 +1,34 @@
 import { useEffect, useRef, useState } from "react";
 import { useSSE } from "../hooks/useSSE";
+import { useAnimate } from "motion/react";
+import { motion } from "motion/react";
 
 export function MusicBox(){  
     const API = import.meta.env.VITE_FASTAPI_URL;
 
+    const [scope, animate] = useAnimate()
     const [trackInfo, setTrackInfo] = useState(null);
+    const [playing, setPlaying] = useState(false);
     const audioRef = useRef(null);
+
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
     const sseURL = `${API}/music/getTrackInfo?curr_id=`;
     const stateURL = `${API}/music/getState`;
 
     const { data: newTrack } = useSSE(sseURL);
     const { data: playData } = useSSE(stateURL);
+
+    useEffect(() => {
+        const run = async () => {
+            await animate(scope.current, {opacity : 1}, {duration: 1.5});
+            await delay(20000);
+            await animate(scope.current, {opacity : 0}, {duration: 1.5});
+        }
+        if (playing){
+            run();
+        }
+    }, [trackInfo, playing]);
 
     useEffect(() => {
         if (newTrack) {
@@ -47,8 +64,12 @@ export function MusicBox(){
         if (playData){
             if (playData == "False"){
                 audio.pause();
+                animate(scope.current, {display: "none"});
+                setPlaying(false);
             } else if (playData == "True"){
                 audio.play().catch(e => console.log("play blocked:", e));
+                animate(scope.current, {display: "flex"});
+                setPlaying(true)
             }
         }
     }, [playData])
@@ -72,10 +93,10 @@ export function MusicBox(){
     return (
         <div className="bg-transparent">
             <audio key={trackInfo?.id} ref={audioRef} src={trackInfo.url}/>
-            <div className="bg-surface text-text text-4xl w-fit max-w-[35%] h-auto border-edge border-3 bottom-0">
+            <motion.div ref={scope} initial={{"opacity": 0}} className="bg-surface text-text text-4xl w-fit max-w-[35%] h-auto border-edge border-3 bottom-0">
                 <p>{trackInfo.title} by {trackInfo.artist}</p>
                 <p>provided by {trackInfo.source}</p>
-            </div>
+            </motion.div>
         </div>
     );
 }
